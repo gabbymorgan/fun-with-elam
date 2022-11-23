d5052772f844e1f1d408eb3a3b780c6673e30f8d
Gabriella Morgan <gabriellapelton@gmail.com>
2022-11-23 09:45:47 -0600

    tree of life

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import Axios from "axios";
+import "./App.css";
 
-function App() {
+function App(props) {
+  const [branches, setBranches] = useState();
+  const [tree, setTree] = useState([]);
+
+  useEffect(() => {
+    return () => {};
+  }, [branches]);
+
+  const makeBranches = () => {};
+
+  const makeTree = (numberOfBranches) => {};
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the Tree of Life. Please choose how many branches you would
+          like.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Tree tree={tree} />
+        <input
+          name="numberOfBranches"
+          onChange={(e) => makeTree(e.target.value)}
+        />
       </header>
     </div>
   );
 }
+
+const Tree = (props) => {
+  //
+};
 
 export default App;
